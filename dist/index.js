3351676959f1c9ed2a7baf0cdb60096460bc1d77
Lemon <bxb100@outlook.com>
2023-07-10 22:40:06 +0800

    chore:

--- dist/index.js
+++ dist/index.js
@@ -57,7 +57,7 @@ class ConfigHelper {
         })
             .filter((option) => option !== '');
         for (const option of provider_options) {
-            const eqIndex = option.indexOf("=");
+            const eqIndex = option.indexOf('=');
             const key = option.slice(0, eqIndex);
             const value = option.slice(eqIndex + 1);
             this._options[key.trim()] = value.trim();
