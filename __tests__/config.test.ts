70361a8eb574206c6735c1491733fc44d77be78a
Lemon <20685961+bxb100@users.noreply.github.com>
2026-03-13 05:43:44 +0800

    chore: remove test debug log (#188)

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -1,10 +1,17 @@
-import { expect, test } from '@jest/globals'
+import { afterEach, describe, expect, it, jest } from '@jest/globals'
 
-import { Config } from '../src/config'
+import * as core from '../__fixtures__/core'
+jest.unstable_mockModule('@actions/core', () => core)
+const { Config } = await import('../src/config')
 
-test('runs', () => {
-  process.env['INPUT_PROVIDER'] = 'memory'
-  process.env['INPUT_PROVIDER_OPTIONS'] = `
+describe('config', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('runs', () => {
+    process.env['INPUT_PROVIDER'] = 'memory'
+    process.env['INPUT_PROVIDER_OPTIONS'] = `
   "A=1 "
 
   B = 2
@@ -15,22 +22,23 @@ test('runs', () => {
   =
   ==invalid==
   `
-  process.env['INPUT_INCLUDE'] =
-    "\
-  './**'\n\
-  '!./exclude'\n\
-  "
-  process.env['INPUT_FLATTEN'] = 'true'
+    process.env['INPUT_INCLUDE'] =
+      "\
+      './**'\n\
+      '!./exclude'\n\
+      "
+    process.env['INPUT_FLATTEN'] = 'true'
 
-  const config = new Config()
-  expect(config.provider).toBe('memory')
-  expect(config.options).toEqual({
-    A: '1',
-    B: '2',
-    C: '3',
-    D: '1=2=3',
-    E: 'E=2'
+    const config = new Config()
+    expect(config.provider).toBe('memory')
+    expect(config.options).toEqual({
+      A: '1',
+      B: '2',
+      C: '3',
+      D: '1=2=3',
+      E: 'E=2'
+    })
+    expect(config.patterns).toEqual(['./**', '!./exclude'])
+    expect(config.flatten).toBe(true)
   })
-  expect(config.patterns).toEqual(['./**', '!./exclude'])
-  expect(config.flatten).toBe(true)
 })
